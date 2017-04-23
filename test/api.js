bbb6442a15077b9f67551abaa710dd5b438b49c3
Alex Gustafsson <alex-gustafsson@live.se>
2017-04-23 20:06:57 +0200

    Fixed line ending, fixed #2, fixed id not being unique

--- test/api.js
+++ test/api.js
@@ -6,10 +6,8 @@ test.cb('Fetching student accommodations', t => {
   api.fetchAccommodations()
   .then(accommodations => {
     t.is(Array.isArray(accommodations), true);
-
     if (accommodations[0]) {
       const accommodation = accommodations[0];
-
       t.is(typeof accommodation, 'object');
       t.is(typeof accommodation.id, 'number');
       t.is(typeof accommodation.rent, 'number');
